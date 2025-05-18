6bbe0aaa8fb209a6fc82532fa856718909ebc7ca
Michael Bennett Cohn <mbcohn@amazon.com>
2025-05-18 01:13:08 +0000

    feat: Add CDK infrastructure for documentation website

---
+++ infrastructure/bin/infrastructure.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { DocumentationWebsiteStack } from '../lib/documentation-website-stack';
+
+const app = new cdk.App();
+new DocumentationWebsiteStack(app, 'QCliDocsWebsiteStack', {
+  env: { 
+    account: process.env.CDK_DEFAULT_ACCOUNT, 
+    region: process.env.CDK_DEFAULT_REGION || 'us-west-2' 
+  },
+  description: 'Amazon Q CLI Documentation Website',
+});
+
+app.synth();
---
+++ infrastructure/lib/documentation-website-stack.ts
@@ -0,0 +1,58 @@
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import { Construct } from 'constructs';
+
+export class DocumentationWebsiteStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create an S3 bucket to store the website content
+    const websiteBucket = new s3.Bucket(this, 'DocumentationBucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // Create an Origin Access Identity for CloudFront
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, 'OriginAccessIdentity');
+    
+    // Grant read permissions to CloudFront
+    websiteBucket.grantRead(originAccessIdentity);
+
+    // Create a CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket, {
+          originAccessIdentity,
+        }),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      // For SPA-like navigation
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+        },
+      ],
+    });
+
+    // Deploy the website content
+    new s3deploy.BucketDeployment(this, 'DeployDocumentation', {
+      sources: [s3deploy.Source.asset('../docs/generated')], // Path to your generated docs
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
+
+    // Output the CloudFront URL
+    new cdk.CfnOutput(this, 'DistributionDomainName', {
+      value: `https://${distribution.distributionDomainName}`,
+      description: 'The domain name of the CloudFront distribution',
+    });
+  }
+}
